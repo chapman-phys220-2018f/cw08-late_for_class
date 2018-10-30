e95e7f09ada8e75ee311ea445e1464c8c20fd0a7
wheatonabigail <wheaton@chapman.edu>
2018-10-30 17:59:32 +0000

    finished cw08

--- sinesum.py
+++ sinesum.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-<<<<<<< HEAD
 ###
 # Name: Abby Wheaton and Frank Entriken
 # Student ID: 2299246, 2298368
@@ -42,17 +41,3 @@ def f(t, T=2*np.pi):
     
     
 
-=======
-import numpy as np
-
-def sinesum(t=.01*2*np.pi, n=1, T = 2*np.pi):
-
-    kmin = 1
-    kmax = n
-    dk = 1
-
-    k = np.arange(kmin, kmax + dk, dk)
-    gk = ((4/np.pi) * (((1 / 2*k-1) * np.sin(2*(2*k-1)*np.pi*t / T))))
-    sum_gk = np.trapz(gk, k, dk)
-    return sum_gk
->>>>>>> 2a803965bbb7ab424468ebf0b3e7615c93094899
